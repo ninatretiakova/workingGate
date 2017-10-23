dc20bf07e45f45a1908a5d0a2355809ed0491c0e
ninatretiakova <32405962+ninatretiakova@users.noreply.github.com>
2017-10-23 00:26:21 -0400

    Gate minmax
    Added min/max functions to automatically calculate the min/max of the axes. Fixed the issue of printing the list of column headers.

--- working_app.R
+++ working_app.R
@@ -1,4 +1,4 @@
-library(shiny)
+#Install and update before starting:
 
 #install.packages("installr")
 #library(installr)
@@ -8,116 +8,133 @@ library(shiny)
 #biocLite("flowCore")
 #install.packages("flowCore")
 
-library("flowCore")
-library("flowViz")
+#library(shiny) #Load shiny library.
+#library("flowCore") #Load flowCore library.
+#library("flowViz")  #Load flowViz library.
 
-mydata = read.FCS('20160810ndbg/B01 control.fcs') # Read an FCS file. 6B pneumococcus.
-E <- exprs(mydata)
-mysample <- E[1:5000, 1:12]
-mymat <- as.data.frame(mysample)
+#setwd("C:/Users/ninat/Documents/") #Set working directory.
 
-names(mymat)[names(mymat)=="FITC-A"] <- "FITC" #rename column
-names(mymat)[names(mymat)=="FSC-A"] <-"FSCA"
-names(mymat)[names(mymat)=="FSC-H"] <-"FSCH"
-names(mymat)[names(mymat)=="FSC-W"] <-"FSCW"
-names(mymat)[names(mymat)=="SSC-A"] <-"SSCA"
-names(mymat)[names(mymat)=="PE-A"] <-"PEA"
-names(mymat)[names(mymat)=="PE-Cy7-A"] <-"PECY7A"
-names(mymat)[names(mymat)=="Pacific Blue-A"] <-"PACBLUEA"
-names(mymat)[names(mymat)=="APC-A"] <-"APCA"
-names(mymat)[names(mymat)=="Alexa Fluor 700-A"] <-"ALEXAFLUOR"
-names(mymat)[names(mymat)=="APC-Cy7-A"] <-"APCCY7"
-names(mymat)[names(mymat)=="ALEXAFLUOR"] <-"ALEXA700"
+#mydata = read.FCS('20160810ndbg/B01 control.fcs') 
 
+#Read an FCS file.
 
-library(shiny)
+mydata = read.FCS('20160810ndbg/B02 24hr-2ndbg-notoxo.fcs')
+
+#Create object with rows as cells and columns as channels.
+
+E <- exprs(mydata) 
+
+#Coerce an object into a data frame.
+
+mymat <- as.data.frame(E) 
+
+colnames(mymat)
+x <- colnames(mymat)
+toString(x)
+
+#########################################
+#App#
+#########################################
 
 ui <- fluidPage(
   
   titlePanel("Gating Example"),
   
-  "Number of entries in the sample: ", nrow(mysample),
-  "\nNumber of channels in the sample: ", ncol(mysample),
-  "\nThe channel names are: ", names(mymat),
-  
   sidebarLayout(
     
-  sidebarPanel(
+    sidebarPanel(
+      
+      
+      #Let the user choose the channels to visualize.
+      
+      selectInput('chan1', "Choose x-axis channel name", colnames(mymat)),
+      selectInput('chan2', "Choose y-axis channel name", colnames(mymat)),
+      
+      #Get the gate limits from the user.
+      
+      sliderInput(inputId = "numx",
+                  label = "Choose x-axis limit",
+                  value = c(200,900), min = 0, max = 250000),
+      sliderInput(inputId = "numy",
+                  label = "Choose y-axis limit",
+                  value = c(200, 3000), min = 0, max = 50000),
+      
+      #Let the user rename the channel names in accordance with the study.
+      
+      selectInput('getchannel', "Choose the channel to rename", colnames(mymat)),
+      textInput('newchannel', "Type new channel name"),
+      
+      #Let the user select the channel to visualize as a histogram.
+      
+      selectInput('gethist', "Select a channel to construct a histogram", colnames(mymat))
+    ),
     
-    #selectInput("dataset","Choose dataset",
-                  	#choices = names(mymat)),
-    sliderInput(inputId = "numx",
-                label = "Choose x-axis limit",
-                value = c(200,900), min = 0, max = 1000000),
-    sliderInput(inputId = "numy",
-                label = "Choose y-axis limit",
-                value = c(200, 3000), min = 0, max = 1000000),
     
-    selectInput("chan", "Choose channel name", names(mymat)),
-    actionButton("newplot", "New plot")
-  ),
+    mainPanel(
+      
+      p("Number of events in the sample: ", nrow(mymat)),
+      p("Number of channels in the sample: ", ncol(mymat)),
+      p("The channel names are: ", toString(x)),
+      p("Use the sliders to set gate limits."),
+      p("You may want to rename the channels in accordance with your study. 
+        Select the channel(s) you wish to rename and type in the new name(s)."),
   
-  mainPanel(
-    plotOutput("plot")
-  )
-  
-  
-  # mainPanel(
-  #   textOutput("channels")
-  # ),
-  # 
-  # mainPanel(
-  #   textOutput("newnames")
-  # )
- )  
+      plotOutput("plot"),
+      plotOutput("hist")
+    )
+  )  
 )
 
-
-#plotOutput("hist")
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  selectedData <- reactive({
+    mymat[, c(input$chan1, input$chan2)]
+  })
   
   sliderValues <- reactive({
     
-    # Compose data frame
+    #Compose data frame.
     data.frame(
       Name = c("numx", "numy"),
       
       Value = c(input$numx,
-               input$numy),
+                input$numy),
       
       stringsAsFactors=FALSE)
   })
 
+  observe({
+    
+    val1 <- mymat[, input$chan1]
+    val2 <- mymat[, input$chan2]
+   
+    updateSliderInput(session, "numx", value = 100, min = min(val1), max = max(val1))
+    updateSliderInput(session, "numy", value = 100, min = min(val2), max = max(val2))
+  })
   
   output$plot <- renderPlot({
-   # , xlim = input$numx, ylim = input$numy
-    View(mysample)
-    plot(mysample[1:5000, 1:2], main = "Test", xlim = input$numx, ylim = input$numy)
+    
+    plot(selectedData(),xlim = input$numx, ylim = input$numy, main = "Gate Area")
+    
+  })
+  
+  selectedChannel <- reactive({
+    mymat[, input$gethist]
+  })
+ 
+  output$hist <- renderPlot({
+    
+    hist(log10(mymat[, input$gethist]), main = "Median Fluorescence Intensity (MFI)", col = "lightblue")
+    #plot(density(log10(mymat[, input$gethist])), main = "Median Fluorescence Intensity (MFI)", toString(x[mymat[, input$gethist]]))
+    abline(v = median(log10(mymat[, input$gethist])), col = 'orange', lwd = 2, lty = "dashed")
+    legend("topleft", legend = paste("MFI:", median(log10(mymat[, input$gethist]))), col = "red", pch = 19) #Median fluorescence intensity
 
   })
   
-  # output$num_row <- renderText({ 
-  #   paste("The number of entries our sample consists of is: ", nrow(mysample))
-  # })
-  # 
-  # output$num_col <- renderText({ 
-  #   paste("The number of available channels is:", ncol(mysample))
-  # })
-  # 
-  # output$channels <- renderText({ 
-  #   paste("The names of available channels are:", names(mymat))
-  # })
-  # 
+  
   # output$newnames <- renderText({ 
   #   paste("Choose new names for the channels.")
   # })
 }
 
 shinyApp(ui = ui, server = server)
-
-##################################
-# PROBLEMS
-##################################
-
-# How to select different channels? Assign column names to respective numbers?
-# Write cases: if I choose channel A, then I will work with such and such columns
